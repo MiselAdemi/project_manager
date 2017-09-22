12158c905c5182b25e1585cda91a8576b523cb53
Ademi Misel <ademimisel@gmail.com>
2017-09-22 17:03:32 +0200

    Add attachments to comments

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise', '~> 4.3'
 gem 'bootstrap', '~> 4.0.0.beta'
+gem 'carrierwave', '~> 1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922122152) do
+ActiveRecord::Schema.define(version: 20170922143827) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commentable_type"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170922122152) do
     t.text     "body"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "attachment"
   end
 
   create_table "projects", force: :cascade do |t|
