76306f877b4251a69d533f23b0af059cafbffe26
Ademi Misel <ademimisel@gmail.com>
2017-09-23 04:17:27 +0200

    Add delete message and refactor comment specs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-  belongs_to :project
-  has_many :comments, as: :commentable
-  
+  belongs_to :project, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+
   enum priority: [:low, :medium, :high]
 
   validates :name, presence: true
--- spec/controllers/tasks_comments_controller_spec.rb
+++ spec/controllers/tasks_comments_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Projects::CommentsController, type: :controller do
+RSpec.describe Tasks::CommentsController, type: :controller do
   before do
     user = FactoryGirl.create(:user)
     allow(request.env['warden']).to receive(:authenticate!).and_return(user)
@@ -9,34 +9,34 @@ RSpec.describe Projects::CommentsController, type: :controller do
 
   describe "POST #create" do
     before do
-      @comment_attr = FactoryGirl.build(:comment, :for_project).attributes
+      @comment_attr = FactoryGirl.build(:comment, :for_task).attributes
     end
 
     it "creates a new comment" do
       expect{
-        post :create, params: { project_id: Project.first, comment: @comment_attr }
+        post :create, params: { project_id: Project.first, task_id: Task.first, comment: @comment_attr }
       }.to change(Comment, :count).by(1)
     end
 
     it "redirects back" do
-      post :create, params: { project_id: Project.first, comment: @comment_attr }
+      post :create, params: { project_id: Project.first, task_id: Task.first, comment: @comment_attr }
       expect(response).to redirect_to(authenticated_root_path)
     end
   end
 
   describe "DELETE #destroy" do
     before do
-      @comment = FactoryGirl.create(:comment, :for_project)
+      @comment = FactoryGirl.create(:comment, :for_task)
     end
 
     it "deletes the comment" do
       expect{
-        delete :destroy, params: { project_id: Project.first, id: @comment }
+        delete :destroy, params: { project_id: Project.first, task_id: Task.first, id: @comment }
       }.to change(Comment, :count).by(-1)
     end
 
     it "redirects to root" do
-      delete :destroy, params: { project_id: Project.first, id: @comment }
+      delete :destroy, params: { project_id: Project.first, task_id: Task.first, id: @comment }
       expect(response).to redirect_to(authenticated_root_path)
     end
   end
